a16d2245cf33eba31a5a6ef0de32aa375767d447
kostiantyn-shyniakov <shinyakov@gmail.com>
2020-10-13 16:31:36 +0300

    Test2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,11 +1,20 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MainClassTest {
+    MainClass mc = new MainClass();
+
     @Test
     public void testGetLocalNumber()
     {
-        MainClass mc = new MainClass();
-        Assert.assertTrue("LocalNumber is not equal to 14", mc.getLocalNumber()==14);
+
+        Assert.assertTrue("LocalNumber does not equal 14", mc.getLocalNumber()==14);
+    }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        Assert.assertTrue("ClassNumber is less or equal 45", mc.getClassNumber()>45);
     }
 }
