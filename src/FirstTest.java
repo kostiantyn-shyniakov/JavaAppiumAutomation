c07724f845a0229e7407192659b50312cae09382
kostiantyn-shyniakov <shinyakov@gmail.com>
2020-10-29 12:17:14 +0200

    L2Ex2

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,15 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -30,8 +36,34 @@ public class FirstTest {
         driver.quit();
     }
     @Test
-    public void firstTest()
+    public void checkTextInSearchContainer()
     {
-        System.out.println("First test");
+        TouchAction touchAction = new TouchAction(driver);
+        touchAction.tap(117, 1710).perform();       //tap 'Skip'
+        By search_container = By.xpath("//*[@resource-id='org.wikipedia:id/search_container']//*[@class='android.widget.TextView']");
+        assertElementHasText(search_container,"Search Wikipedia","Expected text is absent in search container");
+    }
+
+    private WebElement waitForElementPresence(By by, String error_massage, long timeoutInSeconds)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_massage + "\n");
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementPresence(By by, String error_massage)
+    {
+        return waitForElementPresence(
+                by,
+                error_massage,
+                5
+        );
+    }
+
+    private void assertElementHasText(By by, String expected_text, String error_massage){
+        WebElement element = waitForElementPresence(by, "Cannot find TextView element in search container");
+        Assert.assertEquals(error_massage,element.getText(),expected_text);
     }
 }
