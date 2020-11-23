5e1447197b6b1a6544f37b4bb2b4837fcaf176ac
kostiantyn-shyniakov <shinyakov@gmail.com>
2020-11-23 15:28:55 +0200

    L5Ex10 part 1

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -3,6 +3,7 @@ package lib;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 import junit.framework.TestCase;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -15,19 +16,33 @@ public class CoreTestCase extends TestCase {
     @Override
     protected void setUp() throws Exception
     {
+        String platform = System.getenv("PLATFORM");
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        capabilities.setCapability("platformName","Android");
-        capabilities.setCapability("deviceName","AndroidDeviceName");
-        capabilities.setCapability("platformVersion","8.0");
-        capabilities.setCapability("automationName","Appium");
-        capabilities.setCapability("appPackage","org.wikipedia");
-        capabilities.setCapability("appActivity",".main.MainActivity");
-        capabilities.setCapability("orientation","PORTRAIT");
-        capabilities.setCapability("app","C:\\Users\\kshyniakov\\Documents\\GitHub\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
-
-        driver = new AndroidDriver(new URL(AppiumURL), capabilities);
-        clickSkip();
+        if(platform.equals("iOS")){
+            capabilities.setCapability("platformName","iOS");
+            capabilities.setCapability("deviceName","iPhone SE");
+            capabilities.setCapability("platformVersion","13.4");
+            capabilities.setCapability("app","/Users/webteam/Desktop/Wikipedia.app");
+            driver = new IOSDriver(new URL(AppiumURL), capabilities);
+            TouchAction touchAction = new TouchAction(driver);
+            touchAction.tap(69, 749).perform();
+        } else if (platform.equals("android")){
+            capabilities.setCapability("platformName","Android");
+            capabilities.setCapability("deviceName","AndroidDeviceName");
+            capabilities.setCapability("platformVersion","8.0");
+            capabilities.setCapability("automationName","Appium");
+            capabilities.setCapability("appPackage","org.wikipedia");
+            capabilities.setCapability("appActivity",".main.MainActivity");
+            capabilities.setCapability("orientation","PORTRAIT");
+            capabilities.setCapability("app","C:\\Users\\kshyniakov\\Documents\\GitHub\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
+            driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+            TouchAction touchAction = new TouchAction(driver);
+            touchAction.tap(117, 1710).perform();
+        } else {
+            throw new Exception("Cannot get run platform from env veriable");
+        }
+        //clickSkip();
     }
     @Override
     protected void tearDown()
@@ -36,7 +51,6 @@ public class CoreTestCase extends TestCase {
     }
 
     private void clickSkip(){
-        TouchAction touchAction = new TouchAction(driver);
-        touchAction.tap(117, 1710).perform();
+
     }
 }
--- src/lib/ui/MainPageObject.java
+++ src/lib/ui/MainPageObject.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.regex.Pattern;
+
 public class MainPageObject {
     protected AppiumDriver driver;
 
@@ -62,5 +64,18 @@ public class MainPageObject {
     public void assertElementHasText(By by, String expected_text, String error_massage){
         WebElement element = waitForElementPresence(by, "Cannot find TextView element in search container");
         Assert.assertEquals(error_massage,element.getText(),expected_text);
+    }
+
+    private By getLocatorString(String locator_with_type){
+        String[] exploaded_locator = locator_with_type.split(Pattern.quote(":"),2);
+        String by_type = exploaded_locator[0];
+        String locator = exploaded_locator[1];
+        if (by_type.equals("xpath")){
+            return By.xpath(locator);
+        } else if (by_type.equals("id")){
+            return By.id(locator);
+        } else {
+            throw new IllegalArgumentException("Cannot get type of locator. Locator:" + locator_with_type);
+        }
     }
 }
